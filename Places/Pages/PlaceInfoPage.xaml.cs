4142fdb51933de97beb24c556248a325c267ec28
Àíäðåé Òîêàðåâ <andrewtok@sitesoft.ru>
2014-07-22 18:17:02 +0600

    Complite Place Info Card

--- Places/Pages/PlaceInfoPage.xaml.cs
+++ Places/Pages/PlaceInfoPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using Microsoft.Phone.Tasks;
 
 namespace Places.Pages
 {
@@ -20,19 +21,8 @@ namespace Places.Pages
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
-            App.PlaceInfoViewModel = new ViewModel.PlaceInfoViewModel(App.PlaceInfoSearchViewModel.PlaceInfo);
+            App.PlaceInfoViewModel = new ViewModel.PlaceInfoViewModel(App.PlaceInfoSearchViewModel.PlaceInfo, App.ResultPageViewModel.DistancePlace, App.ResultPageViewModel.SelectPlace.Price_level, App.ResultPageViewModel.SelectPlace.Opening_hours.Open_now);
             LayoutRoot.DataContext = App.PlaceInfoViewModel;
-            if (App.PlaceInfoViewModel.PlaceInfo.Reviews == null)
-            {
-                int index = Pivots.Items.Count;
-                Pivots.Items.RemoveAt(index - 2);
-    
-            }
-            if (App.PlaceInfoViewModel.PlaceInfo.Events == null)
-            {
-                int index = Pivots.Items.Count;
-                Pivots.Items.RemoveAt(index - 1);
-            }
         }
 
         protected override void OnBackKeyPress(System.ComponentModel.CancelEventArgs e)
@@ -40,6 +30,36 @@ namespace Places.Pages
             base.OnBackKeyPress(e);
 
             NavigationService.Navigate(new Uri("/Pages/ResultPage.xaml", UriKind.Relative));
+        }
+
+        private void Site_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            WebBrowserTask webBrowserTask = new WebBrowserTask();
+            webBrowserTask.Uri = new Uri(App.PlaceInfoViewModel.PlaceInfo.Website, UriKind.RelativeOrAbsolute);
+            webBrowserTask.Show();
+        }
+
+        private void Google_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            WebBrowserTask webBrowserTask = new WebBrowserTask();
+            webBrowserTask.Uri = new Uri(App.PlaceInfoViewModel.PlaceInfo.Url, UriKind.RelativeOrAbsolute);
+            webBrowserTask.Show();
+        }
+
+        private void FormatNumber_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            PhoneCallTask phoneCall = new PhoneCallTask();
+            phoneCall.PhoneNumber = App.PlaceInfoViewModel.PlaceInfo.Formatted_phone_number;
+            phoneCall.DisplayName = App.PlaceInfoViewModel.PlaceInfo.Name;
+            phoneCall.Show();
+        }
+
+        private void InternationalNumber_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            PhoneCallTask phoneCall = new PhoneCallTask();
+            phoneCall.PhoneNumber = App.PlaceInfoViewModel.PlaceInfo.International_phone_number;
+            phoneCall.DisplayName = App.PlaceInfoViewModel.PlaceInfo.Name;
+            phoneCall.Show();
         }
 
     }
--- Places/ViewModel/PlaceInfoViewModel.cs
+++ Places/ViewModel/PlaceInfoViewModel.cs
@@ -8,10 +8,57 @@ namespace Places.ViewModel
 {
     public class PlaceInfoViewModel : GooglePlacesApi.NotifyPropertyBase
     {
-        public PlaceInfoViewModel(GooglePlacesApi.PlaceInfo placeInfo)
+        public PlaceInfoViewModel(GooglePlacesApi.PlaceInfo placeInfo, string distance, string priceLevel, bool openNow)
         {
             this.placeInfo = placeInfo;
+            this.distance = distance;
+            this.priceLevel = priceLevel;
+            this.openNow = openNow;
         }
+
+        private string distance;
+
+        public string Distance
+        {
+            get
+            {
+                return distance;
+            }
+            set
+            {
+                SetProperty<string>(ref distance, value);
+            }
+        }
+
+        private string priceLevel;
+
+        public string PriceLevel
+        {
+            get
+            {
+                return priceLevel;
+            }
+            set
+            {
+                SetProperty<string>(ref priceLevel, value);
+            }
+        }
+
+        private bool openNow;
+
+        public bool OpenNow
+        {
+            get
+            {
+                return openNow;
+            }
+            set
+            {
+                SetProperty<bool>(ref openNow, value);
+            }
+        }
+        
+
 
         private GooglePlacesApi.PlaceInfo placeInfo;
 
